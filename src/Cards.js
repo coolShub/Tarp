da091f9a697601f5b79c2cce226d2824655f27e0
Shubham Singh <singhshubham7m@gmail.com>
2021-05-18 17:56:51 +0530

    second commit

--- src/Cards.js
+++ src/Cards.js
@@ -6,27 +6,28 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check our services!</h1>
+      <h1>Not disabled,just differently-abled</h1>
+      <h3>It doesn't define who you are!!</h3>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           
           <ul className='cards__items'>
             <CardItem
               src="https://raw.githubusercontent.com/coolShub/Enable/main/src/images/deaf.jpg"
-              text='Unable to hear? No problem'
-              label='Speech'
-              path='/speech'
-            />
-            <CardItem
-              src="https://raw.githubusercontent.com/coolShub/Enable/main/src/images/blind.jpg"
-              text='Unable to see?Even then No Problem'
-              label='caption'
+              text='Having difficulty in seeing,use our Image Caption Generator '
+              label='IMAGE-CAPTION-GENERATOR'
               path='/caption'
             />
             <CardItem
+              src="https://raw.githubusercontent.com/coolShub/Enable/main/src/images/blind.jpg"
+              text='Having difficulty in listening,use our Speech-To-Text Module'
+              label='SPEECH-TO-TEXT'
+              path='/speech'
+            />
+            <CardItem
               src="https://raw.githubusercontent.com/coolShub/Enable/main/src/images/signlang.jpg"
-              text='deaf'
-              label='sign'
+              text='Having difficulty in communications,talk via Sign Language'
+              label='SIGN-LANGUAGE GENERATOR'
               path='/sign'
             />
           </ul>
--- src/HeroSection.js
+++ src/HeroSection.js
@@ -5,8 +5,9 @@ function HeroSection() {
   return (
     <div className='hero-container'>
     <img className="hero_back" src="https://alaskacf.org/wp-content/uploads/2018/07/iStock-463406923.jpg"/>
-      <h1>Let's Begin</h1>
-      <p>Service to others is the rent you pay for your room here on earth</p>
+    <h1>Join us on our journey to help the disabled by ENABLING them.....</h1>
+      <p>We are all human beings and thus should be treated similarly. Everyone should be respected equally and should be given equal opportunity. Society is not just a place where we live. It’s an environment we create for everyone. It’s not 
+        the people with disabilities who retreat the society, it’s the mentality that is dragging behind.</p>
       <div className='hero-btns'>
       </div>
     </div>
